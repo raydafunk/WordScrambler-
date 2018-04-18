35fb22e751d1e77827bcce4df1b7ef337e32f730
Ray <r.brown-amory@sky.com>
2018-04-18 13:43:46 +0100

    Add files via upload

---
+++ WordUnScrambler.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WordUnScrambler.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WordUnScrambler.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93719051-066f-42f3-a3eb-8f05d3cdaa0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WordUnScrambler.Test.Unit/WordMatcherTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordScrambler.Helpers;
+
+namespace WordUnScrambler.Test.Unit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private readonly WordMatcher _wordMatcher = new WordMatcher();
+
+        [TestMethod]
+        public void ScrambledwordMatchesGivenWordInList()
+        {
+            string[] words = { "cat", "chair", "more" };
+            string[] scramlbewords = { "omre" };
+
+            var matchedWords = _wordMatcher.Match(scramlbewords , words);
+
+            Assert.IsTrue(matchedWords.Count == 1);
+            Assert.IsTrue(matchedWords[0].ScrambledWord.Equals("omre"));
+            Assert.IsTrue(matchedWords[0].Word.Equals("more"));
+        }
+
+        [TestMethod]
+        public void ScrambledwordMatchesGivenWordsInList()
+        {
+            string[] words = { "cat", "chair", "more" };
+            string[] scramlbewords = { "omre", "act"};
+
+            var matchedWords = _wordMatcher.Match(scramlbewords, words);
+
+            Assert.IsTrue(matchedWords.Count == 2);
+            Assert.IsTrue(matchedWords[0].ScrambledWord.Equals("omre"));
+            Assert.IsTrue(matchedWords[0].Word.Equals("more"));
+            Assert.IsTrue(matchedWords[1].ScrambledWord.Equals("act"));
+            Assert.IsTrue(matchedWords[1].Word.Equals("cat"));
+        }
+    }
+}
